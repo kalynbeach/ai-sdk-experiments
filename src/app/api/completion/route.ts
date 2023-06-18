cfaebfa2b2141b815e2cca1da739c6a9f52fc826
kalynbeach <kalynbeach@gmail.com>
2023-06-18 15:13:55 -0700

    add initial completion component and route handler

---
+++ src/app/api/completion/route.ts
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(config)
+
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json()
+
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    stream: true,
+    temperature: 0.7,
+    prompt
+  })
+
+  const stream = OpenAIStream(response)
+
+  return new StreamingTextResponse(stream)
+}
