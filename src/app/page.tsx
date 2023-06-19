0c2d77380738ad8fd810a2f110edc71914be7c2c
kalynbeach <kalynbeach@gmail.com>
2023-06-18 17:50:50 -0700

    adjust page styles, update RootLayout metadata and README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Completion from './Completion'
 export default function Home() {
   return (
     <main className='min-h-screen p-4 md:p-12 flex flex-col justify-between'>
-      <div className='flex flex-col md:flex-row justify-start items-center gap-4'>  
+      <div className='flex flex-col md:flex-row justify-center items-center gap-4'>  
         <Chat />
         <Completion />  
       </div>
